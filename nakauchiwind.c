f76230f349b978e7b0c613b07aa0de1917650c3f
kazumi kashiyama <k.kashiyama1120@gmail.com>
2019-05-20 17:31:58 +0900

    minor update

--- nakauchiwind.c
+++ nakauchiwind.c
@@ -5,6 +5,21 @@
 double v_beta(double v_inf, double R0, double r, double beta);
 void set_r(double R0, double Rmax, int rbin, double r[]);
 void set_Mr(double Mc, double Mtot, int rbin, double Mr[]);
+double Pgas(double mu, double rho, double T);
+double Prad(double T);
+void dr_dP_and_dLr(double dMr, double Mr, double r, double rho, double epsnu, double *dr, double *dP, double *dLr);
+void dT(double r, double T, double rho, double dr, double Lrad, double kappa, double *dT);
+void drho(double mu, double T, double rho, double dP, double dT, double *drho);
+
+
+/* physical constant */
+const double mol = 6.02e23;
+const double joul_to_erg = 1.e7;
+const double Rgas = 8.314462618*joul_to_erg/mol;
+const double arad = 7.5657e-15;
+const double G = 6.67408e-8;
+const double C = 2.99792458e10;
+const double Msun = 2.e33;
 
 
 /* inner boundary condition */
@@ -12,7 +27,14 @@ const double rhoc = 1.0e9;
 const double Tc = 1.0e8;
 
 
-int main(){
+/* simplification */
+const double mu = 2.;
+const double kappa_fix = 0.4;
+const double epsnu_fix = 0.;
+
+
+int main()
+{
     FILE *op;
     
     double v_inf = 1.e9;
@@ -22,14 +44,14 @@ int main(){
     //printf("%12.3e \n",v_beta(v_inf,R0,r_tmp,beta));
 
 
-    double Rmax = 1.e13;
+    double Rmax = 1.e12;
     int rbin = 100;
     double r[rbin];
     set_r(R0,Rmax,rbin,r);
     
     double drc = 1.e5;
     double Mc = 4.*M_PI/3.*pow(drc,3.);
-    double Mtot = 2.e33;
+    double Mtot = 10.*Msun;
     double Mr[rbin];
     set_Mr(Mc,Mtot,rbin,Mr);
 
@@ -46,11 +68,9 @@ int main(){
 
 /* let me first reproduce Nakauchi et al.18 */
 
-double v_beta(double v_inf, double R0, double r, double beta){
-    return v_inf*pow(1.-R0/r,beta);
-}
-
-void set_r(double R0, double Rmax, int rbin, double r[]){
+/* static core */
+void set_r(double R0, double Rmax, int rbin, double r[])
+{
     double del_ln_r = log(Rmax/R0)/(double)(rbin-1);
     int i;
     for (i=0; i<rbin; i++) {
@@ -58,7 +78,8 @@ void set_r(double R0, double Rmax, int rbin, double r[]){
     }
 }
 
-void set_Mr(double Mc, double Mtot, int rbin, double Mr[]){
+void set_Mr(double Mc, double Mtot, int rbin, double Mr[])
+{
     double del_ln_r = log(Mtot/Mc)/(double)(rbin-1);
     int i;
     for (i=0; i<rbin; i++) {
@@ -67,6 +88,38 @@ void set_Mr(double Mc, double Mtot, int rbin, double Mr[]){
 }
 
 
+double Pgas(double mu, double rho, double T)
+{
+    return Rgas/mu*rho*T;
+}
+
+double Prad(double T)
+{
+    return arad*pow(T,4.)/3.;
+}
+
+void dr_dP_and_dLr(double dMr, double Mr, double r, double rho, double epsnu, double *dr, double *dP, double *dLr)
+{
+    *dr = 1./4./M_PI/r/r/rho*dMr;
+    *dP = -G*Mr/4./M_PI/pow(r,4.);
+    *dLr = epsnu*dMr;
+}
+
+void dT(double r, double T, double rho, double dr, double Lrad, double kappa, double *dT)
+{
+    *dT = -Lrad/(16.*M_PI*arad*C*r*r*pow(T,3.)/3./kappa/rho);
+}
+
+void drho(double mu, double T, double rho, double dP, double dT, double *drho)
+{
+    *drho = mu/Rgas/T*(dP-dT*(Rgas/mu*rho+4./3.*arad*pow(T,3.)));
+}
 
 
 
+
+/* wind region */
+double v_beta(double v_inf, double R0, double r, double beta)
+{
+    return v_inf*pow(1.-R0/r,beta);
+}
