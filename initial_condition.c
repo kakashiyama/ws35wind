fee6b4cb0d80ab5f177949ea70085d60b3031aed
Kazumi Kashiyama <k.kashiyama1120@gmail.com>
2019-05-24 07:41:37 +0900

    minor update

--- initial_condition.c
+++ initial_condition.c
@@ -16,40 +16,49 @@ const double Yr = 365.*24.*60.*60.;
 /* model parameters */
 const double mu = 2.0;
 const double Mwd = 1.0*Msun;
-const double Rwd = 1.0e8;
+const double Rwd = 3.0e8;
 const double Bwd = 1.0e9;
 const double Omega = 0.01;
-const double Mdot = 1.0e6*Msun/Yr;
+const double Mdot = 1.0e-6*Msun/Yr;
 
-void set_initial_guess_at_rA(double rA_tmp, double vA_tmp, double dudxA_tmp, double *BrA, double *rhoA, double *vphiA);
+double michel_wind_velocity(double r, double br, double omega, double mdot);
+void set_initial_guess_at_rA(double rA_tmp, double dudxA_tmp, double *vA, double *BrA, double *rhoA, double *vphiA);
 
 int main()
 {
+    double VM = michel_wind_velocity(Rwd,Bwd,Omega,Mdot);
+    printf("VM = %12.3e cm/s \n",VM);
+    
     double rA_tmp=0.1*Rsun;
-    double fac_W=1.5;
     double dudxA_tmp=1.5;
-    
     //double TA_tmp=1.0e5;
-
-    double vA_tmp=rA_tmp*Omega/fac_W;
     
-    double BrA,rhoA,vphiA;
+    double vA,BrA,rhoA,vphiA;
     
-    set_initial_guess_at_rA(rA_tmp,vA_tmp,dudxA_tmp,&BrA,&rhoA,&vphiA);
-    printf("%12.3e %12.3e %12.3e %12.3e \n",vA_tmp,BrA,rhoA,vphiA);
+    set_initial_guess_at_rA(rA_tmp,dudxA_tmp,&vA,&BrA,&rhoA,&vphiA);
+    printf("%12.3e %12.3e %12.3e %12.3e \n",vA,BrA,rhoA,vphiA);
     return 0;
 }
 
-/* rAとdudxAとLambda、Tを振って... */
-
-void set_initial_guess_at_rA(double rA_tmp, double vA_tmp, double dudxA_tmp, double *BrA, double *rhoA, double *vphiA)
+double michel_wind_velocity(double r, double br, double omega, double mdot)
 {
-    double BrA_tmp = pow(rA_tmp/Rwd,-2.0)*Bwd;
-    double rhoA_tmp = Mdot/4./M_PI/vA_tmp/pow(rA_tmp,2.0);
-    double vphiA_tmp = rA_tmp*Omega*dudxA_tmp/(2.+dudxA_tmp);
+    return pow(pow(r,4.)*pow(br,2.)*pow(omega,2.)/mdot,1./3.);
+}
+
+void set_initial_guess_at_rA(double rA_tmp, double dudxA_tmp, double *vA, double *BrA, double *rhoA, double *vphiA)
+{
+    double VM = michel_wind_velocity(Rwd,Bwd,Omega,Mdot);
+    double fac_W = 1.5;
     
-    *BrA = BrA_tmp;
+    double vA_tmp = fac_W*rA_tmp*Omega;//pow(VM,3.)/pow(rA_tmp*Omega,2.);
+    double vphiA_tmp = rA_tmp*Omega*dudxA_tmp/(2.+dudxA_tmp);
+    double rhoA_tmp = Mdot/4./M_PI/vA_tmp/pow(rA_tmp,2.0);
+    double BrA_tmp = pow(rA_tmp/Rwd,-2.0)*Bwd;
+    
+    
+    *vA = vA_tmp;
+    *vphiA = vphiA_tmp;]
     *rhoA = rhoA_tmp;
-    *vphiA = vphiA_tmp;
+    *BrA = BrA_tmp;
     
 }
