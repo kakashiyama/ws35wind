e474805d4e13e121df8cb70c011805201383c871
kazumi kashiyama <k.kashiyama1120@gmail.com>
2019-05-31 09:20:25 +0900

    tidying up
    find a solution for a fixed kappa

--- shooting_from_rA_to_rwd_debugging.c
+++ shooting_from_rA_to_rwd_debugging.c
@@ -28,21 +28,22 @@ const int rbin = 1000000;
 
 /* shooting trial number */
 const int max_trial = 50;
-const int max_trial_2 = 2;
+const int max_trial_2 = 100;
 
 /* optcaity table #46 */
 const int index_T=58;
 const int index_R=20;
 
 void set_r_from_rA_to_rWD(double rA, double r[]);
-void set_para_at_rA(double rA_tmp, double vA_tmp, double dudxA_tmp, double TA_tmp, double LrA_tmp,
-                    double *BrA, double *rhoA, double *vphiA, double *BphiA, double *Fm, double *FB, double *Lang, double *etot);
+void set_para_at_rA(double rA, double dudxA, double TA, double LrA,
+                    double *vA, double *BrA, double *rhoA, double *vphiA, double *BphiA, double *Fm, double *FB, double *Lang, double *etot);
 void calc_dTdr(double Tinput, double *Toutput, double dr, double r, double rho, double Lr, double kappa);
 void calc_dVrdr_1ststep(double vrinput, double *vroutput, double dr, double dudxA, double vA, double rA);
 void calc_dVrdr_2ststep_and_more(double vrinput, double r, double dr, double rho, double Br, double Bphi, double vphi, double T, double Lr, double kappa,
                                  double *vroutput, double *denominator_of_dvrdr, double *numerator_of_dvrdr);
-void solve_constraint_eqs(double r, double rA, double vA, double vr, double T, double Fm, double FB, double Lang, double etot,
-                          double *Br, double *Bphi, double *vphi, double *Lr, double *rho, double *a);
+void solve_constraint_eqs(double r, double vr, double T,
+                          double rA, double vA, double Fm, double FB, double Lang, double etot,
+                          double *Br, double *Bphi, double *vphi, double *Lr, double *rho);
 
 void load_kappa_table(double kappa_tab[index_T][index_R]);
 double kappa_fit(double log10T, double log10rho, double kappa_tab[index_T][index_R]);
@@ -57,13 +58,14 @@ int main()
     load_kappa_table(kappa_tab);
     
     /* fixed parameters */
-    double vA;
     double dudxA = 3.0;
     double TA = 3.e5;
     double LrA = 2.e38;
     
     /* to-be-calculated qunatities */
-    double BrA,rhoA,vphiA,BphiA,Fm,FB,Lang,etot,dr,a;
+    double vA,BrA,rhoA,vphiA,BphiA; /* at rA */
+    double Fm,FB,Lang,etot; /* conserved quantities */
+    double dr,drA;
     double *r,*vr,*T,*Br,*Bphi,*vphi,*Lr,*rho,*denominator_of_dvrdr,*numerator_of_dvrdr;
     r = (double *)malloc(rbin * sizeof(double));
     vr = (double *)malloc(rbin * sizeof(double));
@@ -76,12 +78,10 @@ int main()
     denominator_of_dvrdr = (double *)malloc(rbin * sizeof(double));
     numerator_of_dvrdr = (double *)malloc(rbin * sizeof(double));
     
-    
     /* 1st trial */
     double ln_rAmax = log(10.*Rsun);
     double ln_rAmin = log(0.01*Rsun);
     double rA = exp(.5*(ln_rAmax+ln_rAmin));
-    vA = pow(Bwd,2.)*pow(Rwd,4.)/Mdot/rA/rA;
     int j=0;
     
     while (j<max_trial){
@@ -91,7 +91,7 @@ int main()
         set_r_from_rA_to_rWD(rA,r);
         
         /* calculate other parameters at rA */
-        set_para_at_rA(rA,vA,dudxA,TA,LrA,&Br[0],&rho[0],&vphi[0],&Bphi[0],&Fm,&FB,&Lang,&etot);
+        set_para_at_rA(rA,dudxA,TA,LrA,&vA,&Br[0],&rho[0],&vphi[0],&Bphi[0],&Fm,&FB,&Lang,&etot);
         vr[0] = vA;
         T[0] = TA;
         Lr[0] = LrA;
@@ -103,7 +103,7 @@ int main()
         calc_dVrdr_1ststep(vr[0],&vr[1],dr,dudxA,vA,rA);
         denominator_of_dvrdr[1] = dudxA*vA*vA;
         numerator_of_dvrdr[1] = vA*rA;
-        solve_constraint_eqs(r[1],rA,vA,vr[1],T[1],Fm,FB,Lang,etot,&Br[1],&Bphi[1],&vphi[1],&Lr[1],&rho[1],&a);
+        solve_constraint_eqs(r[1],vr[1],T[1],rA,vA,Fm,FB,Lang,etot,&Br[1],&Bphi[1],&vphi[1],&Lr[1],&rho[1]);
         kappa[1] = kappa_fit(log10(T[1]),log10(rho[1]),kappa_tab);
         
         /* calculate the 2nd step and more */
@@ -112,11 +112,10 @@ int main()
             dr = r[i+1]-r[i];
             calc_dTdr(T[i],&T[i+1],dr,r[i],rho[i],Lr[i],kappa[i]);
             calc_dVrdr_2ststep_and_more(vr[i],r[i],dr,rho[i],Br[i],Bphi[i],vphi[i],T[i],Lr[i],kappa[i],&vr[i+1],&denominator_of_dvrdr[i+1],&numerator_of_dvrdr[i+1]);
-            solve_constraint_eqs(r[i+1],rA,vA,vr[i+1],T[i+1],Fm,FB,Lang,etot,&Br[i+1],&Bphi[i+1],&vphi[i+1],&Lr[i+1],&rho[i+1],&a);
+            solve_constraint_eqs(r[i+1],vr[i+1],T[i+1],rA,vA,Fm,FB,Lang,etot,&Br[i+1],&Bphi[i+1],&vphi[i+1],&Lr[i+1],&rho[i+1]);
             kappa[i+1] = kappa_fit(log10(T[i+1]),log10(rho[i+1]),kappa_tab);
             i++;
         }
-        
         
         /* set next trial rA */
         if (i >= rbin-1) {
@@ -125,14 +124,15 @@ int main()
             } else {
                 ln_rAmin = log(rA);
                 rA = exp(.5*(log(rA)+ln_rAmax));
-                printf("Up!\n");
+                drA = rA - exp(ln_rAmin);
+                printf("increase rA \n");
             }
         } else {
             ln_rAmax = log(rA);
             rA = exp(.5*(log(rA)+ln_rAmin));
-            printf("Down!\n");
+            drA = exp(ln_rAmin)-rA;
+            printf("decrease rA \n");
         }
-        vA = pow(Bwd,2.)*pow(Rwd,4.)/Mdot/rA/rA;
         
         j++;
     
@@ -155,11 +155,7 @@ int main()
     fclose(op);
     
     
-    
-    
     /* 2nd trial */
-    double eps_rA = 1.0e-8*rA;
-    double rA_max = rA;
     j=0;
     while (j<max_trial_2){
         printf("2nd trial No. %d: rA = %lf cm \n",j,rA);
@@ -168,7 +164,7 @@ int main()
         set_r_from_rA_to_rWD(rA,r);
         
         /* calculate other parameters at rA */
-        set_para_at_rA(rA,vA,dudxA,TA,LrA,&Br[0],&rho[0],&vphi[0],&Bphi[0],&Fm,&FB,&Lang,&etot);
+        set_para_at_rA(rA,dudxA,TA,LrA,&vA,&Br[0],&rho[0],&vphi[0],&Bphi[0],&Fm,&FB,&Lang,&etot);
         vr[0] = vA;
         T[0] = TA;
         Lr[0] = LrA;
@@ -180,48 +176,46 @@ int main()
         calc_dVrdr_1ststep(vr[0],&vr[1],dr,dudxA,vA,rA);
         denominator_of_dvrdr[1] = dudxA*vA*vA;
         numerator_of_dvrdr[1] = vA*rA;
-        solve_constraint_eqs(r[1],rA,vA,vr[1],T[1],Fm,FB,Lang,etot,&Br[1],&Bphi[1],&vphi[1],&Lr[1],&rho[1],&a);
+        solve_constraint_eqs(r[1],vr[1],T[1],rA,vA,Fm,FB,Lang,etot,&Br[1],&Bphi[1],&vphi[1],&Lr[1],&rho[1]);
         kappa[1] = kappa_fit(log10(T[1]),log10(rho[1]),kappa_tab);
         
         /* calculate the 2nd step and more */
         int i=1;
-        while (i<rbin-1 /*&& denominator_of_dvrdr[i] > 0. && numerator_of_dvrdr[i] > 0.*/) {
+        while (i<rbin-1 && denominator_of_dvrdr[i] > 0. && numerator_of_dvrdr[i] > 0.) {
             dr = r[i+1]-r[i];
             calc_dTdr(T[i],&T[i+1],dr,r[i],rho[i],Lr[i],kappa[i]);
             calc_dVrdr_2ststep_and_more(vr[i],r[i],dr,rho[i],Br[i],Bphi[i],vphi[i],T[i],Lr[i],kappa[i],&vr[i+1],&denominator_of_dvrdr[i+1],&numerator_of_dvrdr[i+1]);
-            solve_constraint_eqs(r[i+1],rA,vA,vr[i+1],T[i+1],Fm,FB,Lang,etot,&Br[i+1],&Bphi[i+1],&vphi[i+1],&Lr[i+1],&rho[i+1],&a);
+            solve_constraint_eqs(r[i+1],vr[i+1],T[i+1],rA,vA,Fm,FB,Lang,etot,&Br[i+1],&Bphi[i+1],&vphi[i+1],&Lr[i+1],&rho[i+1]);
             kappa[i+1] = kappa_fit(log10(T[i+1]),log10(rho[i+1]),kappa_tab);
             i++;
         }
         
-        
         /* set next trial rA */
         if (denominator_of_dvrdr[i]*numerator_of_dvrdr[i]>0.){
-            printf("Yahoo!\n");
+            printf("find a solution!\n");
             break;
         } else if (denominator_of_dvrdr[i] <=0.) {
-            rA -= eps_rA;
-            printf("Down!\n");
+            rA -= drA;
+            printf("decrease rA \n");
         } else {
-            rA -= eps_rA;
-            printf("Give Up!\n");
-            //break;
+            rA += drA;
+            drA = 0.1*drA;
+            printf("increase rA \n");
         }
-        vA = pow(Bwd,2.)*pow(Rwd,4.)/Mdot/rA/rA;
         
         j++;
         
     }
     
+    
     op = fopen("test2.dat","w");
     for (j=0; j<rbin; j++) {
-        //if (j % 100 == 0){
+        if (j % 100 == 0){
             fprintf(op,"%12.7e %12.7e %12.7e %12.7e %12.7e %12.7e %12.7e %12.7e %12.7e %12.7e %12.7e \n",
                     r[j],vr[j],T[j],Br[j],Bphi[j],vphi[j],Lr[j],rho[j],kappa[j],denominator_of_dvrdr[j],numerator_of_dvrdr[j]);
-        //}
+        }
     }
     fclose(op);
-    
     
     
     free(r);
@@ -248,20 +242,22 @@ void set_r_from_rA_to_rWD(double rA, double r[])
     }
 }
 
-void set_para_at_rA(double rA, double vA, double dudxA, double TA, double LrA,
-                    double *BrA, double *rhoA, double *vphiA, double *BphiA, double *Fm, double *FB, double *Lang, double *etot)
+void set_para_at_rA(double rA, double dudxA, double TA, double LrA,
+                    double *vA, double *BrA, double *rhoA, double *vphiA, double *BphiA, double *Fm, double *FB, double *Lang, double *etot)
 {
+    double vA_tmp = pow(Bwd,2.)*pow(Rwd,4.)/Mdot/rA/rA;
     double BrA_tmp = pow(rA/Rwd,-2.0)*Bwd;
-    double rhoA_tmp = Mdot/4./M_PI/vA/pow(rA,2.0);
+    double rhoA_tmp = Mdot/4./M_PI/vA_tmp/pow(rA,2.0);
     double vphiA_tmp = rA*Omega*dudxA/(2.+ dudxA);
-    double BphiA_tmp = -BrA_tmp*rA*Omega/vA*(2./(2.+ dudxA));
+    double BphiA_tmp = -BrA_tmp*rA*Omega/vA_tmp*(2./(2.+ dudxA));
     
     double hA = 5./2.*kB*TA/mu_mol/Mu + 4.*arad*pow(TA,4.)/3./rhoA_tmp;
-    double kA = 0.5*(vA*vA + vphiA_tmp*vphiA_tmp);
-    double Fm_tmp = rhoA_tmp*vA*rA*rA;
+    double kA = 0.5*(vA_tmp*vA_tmp + vphiA_tmp*vphiA_tmp);
+    double Fm_tmp = rhoA_tmp*vA_tmp*rA*rA;
     double Lang_tmp = rA*rA*Omega;
     double etot_tmp = LrA/4./M_PI/Fm_tmp + kA + hA - G*Mwd/rA - rA*Omega*vphiA_tmp + Lang_tmp*Omega;
    
+    *vA = vA_tmp;
     *BrA = BrA_tmp;
     *rhoA = rhoA_tmp;
     *vphiA = vphiA_tmp;
@@ -276,7 +272,7 @@ void set_para_at_rA(double rA, double vA, double dudxA, double TA, double LrA,
 
 void calc_dTdr(double Tinput, double *Toutput, double dr, double r, double rho, double Lr, double kappa)
 {
-    *Toutput = Tinput - 3.*kappa*rho*Lr/16./M_PI/arad/C/pow(Tinput,3.)/pow(r,2.)*dr;
+    *Toutput = Tinput - dr*3.*kappa*rho*Lr/16./M_PI/arad/C/pow(Tinput,3.)/pow(r,2.);
 }
 
 
@@ -292,7 +288,7 @@ void calc_dVrdr_2ststep_and_more(double vrinput, double r, double dr, double rho
     double Ar = Br/sqrt(4.*M_PI*rho);
     double Aphi = Bphi/sqrt(4.*M_PI*rho);
     
-    double denominator_of_dvrdr_tmp = (vrinput*vrinput -kB*T/mu_mol/Mu - Aphi*Aphi*vrinput*vrinput/(vrinput*vrinput-Ar*Ar))*r/vrinput;
+    double denominator_of_dvrdr_tmp = (vrinput*vrinput - kB*T/mu_mol/Mu - Aphi*Aphi*vrinput*vrinput/(vrinput*vrinput-Ar*Ar))*r/vrinput;
     double dPdr_term = 3.*kappa*Lr/16./M_PI/arad/C/pow(T,3.)/r*(rho*kB/mu_mol/Mu + 4./3.*arad*pow(T,3.)) + 2.*kB*T/mu_mol/Mu;
     double gravity_term = -G*Mwd/r;
     double centrifugal_force_term = vphi*vphi;
@@ -305,8 +301,9 @@ void calc_dVrdr_2ststep_and_more(double vrinput, double r, double dr, double rho
 }
 
 
-void solve_constraint_eqs(double r, double rA, double vA, double vr, double T, double Fm, double FB, double Lang, double etot,
-                          double *Br, double *Bphi, double *vphi, double *Lr, double *rho, double *a)
+void solve_constraint_eqs(double r, double vr, double T,
+                          double rA, double vA, double Fm, double FB, double Lang, double etot,
+                          double *Br, double *Bphi, double *vphi, double *Lr, double *rho)
 {
     double Br_tmp = FB/r/r;
     double rho_tmp = Fm/vr/r/r;
@@ -316,17 +313,15 @@ void solve_constraint_eqs(double r, double rA, double vA, double vr, double T, d
     double Bphi_tmp = -Br_tmp*rA*Omega/vA*x*(1.-x*x)/(1.-x*x*u);
     double a_tmp = sqrt(kB*T/mu_mol/Mu);
 
-    
     double h_tmp = 5./2.*kB*T/mu_mol/Mu + 4.*arad*pow(T,4.)/3./rho_tmp;
-    double k_tmp = 0.5*(vr*vr+vphi_tmp*vphi_tmp);
-    double Lr_tmp = 4.*M_PI*Fm*(etot-k_tmp-h_tmp+G*Mwd/r+r*Omega*vphi_tmp-Lang*Omega);
+    double k_tmp = 0.5*(vr*vr + vphi_tmp*vphi_tmp);
+    double Lr_tmp = 4.*M_PI*Fm*(etot - k_tmp - h_tmp + G*Mwd/r + r*Omega*vphi_tmp - Lang*Omega);
     
     *Br = Br_tmp;
     *Bphi = Bphi_tmp;
     *vphi = vphi_tmp;
     *Lr = Lr_tmp;
     *rho = rho_tmp;
-    *a = a_tmp;
 }
 
 
@@ -370,7 +365,7 @@ double kappa_fit(double log10T, double log10rho, double kappa_tab[index_T][index
     double kappa_Tdirect_max = (kappa_tab[index_T_fit][index_R_fit]-kappa_tab[index_T_fit][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit][index_R_fit-1];
     double log10kappa = (kappa_Tdirect_max-kappa_Tdirect_min)/(kappa_tab[index_T_fit][0]-kappa_tab[index_T_fit-1][0])*(log10T-kappa_tab[index_T_fit-1][0])+kappa_Tdirect_min;
     
-    return 0.2;//pow(10.,log10kappa);
+    return pow(10.,log10kappa);
 }
 
 
