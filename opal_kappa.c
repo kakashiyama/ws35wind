bd8f20efae0fba77b8d80998bb8c22bd6c6c9eaf
kazumi kashiyama <k.kashiyama1120@gmail.com>
2019-06-03 14:11:44 +0900

    opal_kappa update

--- opal_kappa.c
+++ opal_kappa.c
@@ -4,6 +4,7 @@
 /* see table #46 */
 const int index_T=58;
 const int index_R=20;
+const int index_table_tot = 71*20;
 
 void load_kappa_table(double kappa_tab[index_T][index_R]);
 double kappa_fit(double log10T6, double log10rho, double kappa_tab[index_T][index_R]);
@@ -37,31 +38,79 @@ void load_kappa_table(double kappa_tab[index_T][index_R])
 }
 
 
+/* old version */
+//double kappa_fit(double log10T, double log10rho, double kappa_tab[index_T][index_R])
+//{
+//    /* bilinear extrapolation of the OPAL opacity table */
+//    int i,j,index_T_fit=0,index_R_fit=0;
+//    double log10R = log10rho-3.*(log10T-6.);
+//
+//    i=1;
+//    while(kappa_tab[i][0]<log10T && i<index_T-1)
+//        i++;
+//    index_T_fit = i;
+//
+//    if (index_T_fit == 1){
+//        index_T_fit = 2;
+//        log10T = kappa_tab[1][0]
+//    } else if (index_T_fit == index_T-1){
+//        log10T = kappa_tab[index_T-1][0];
+//    }
+//
+//    j=1;
+//    while(kappa_tab[0][j]<log10R && j<index_R-1)
+//        j++;
+//    index_R_fit = j;
+//
+//    if (index_R_fit == 1)
+//        index_R_fit = 2;
+//    /* tableの外側にはバカ外挿 */
+//
+//    double kappa_Tdirect_min = (kappa_tab[index_T_fit-1][index_R_fit]-kappa_tab[index_T_fit-1][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit-1][index_R_fit-1];
+//    double kappa_Tdirect_max = (kappa_tab[index_T_fit][index_R_fit]-kappa_tab[index_T_fit][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit][index_R_fit-1];
+//    double log10kappa = (kappa_Tdirect_max-kappa_Tdirect_min)/(kappa_tab[index_T_fit][0]-kappa_tab[index_T_fit-1][0])*(log10T-kappa_tab[index_T_fit-1][0])+kappa_Tdirect_min;
+//
+//    return pow(10.,log10kappa);
+//}
+
+
 double kappa_fit(double log10T, double log10rho, double kappa_tab[index_T][index_R])
 {
     /* bilinear extrapolation of the OPAL opacity table */
     int i,j,index_T_fit=0,index_R_fit=0;
     double log10R = log10rho-3.*(log10T-6.);
-    
+
     i=1;
-    while(kappa_tab[i][0]<log10T && i<index_T-1)
+    while(kappa_tab[i][0]<log10T && i<index_T)
         i++;
     index_T_fit = i;
-    if (index_T_fit == 1)
+
+    if (index_T_fit == 1){
+        //printf("Warning (T too small for opal kappa): log10T = %le --> %le , log10R = %le \n",log10T,kappa_tab[1][0],log10R);
         index_T_fit = 2;
+        log10T = kappa_tab[1][0];
+    } else if (index_T_fit == index_T){
+        //printf("Warning (T too large for opal kappa): log10T = %le --> %le , log10R = %le \n",log10T,kappa_tab[index_T-1][0],log10R);
+        log10T = kappa_tab[index_T-1][0];
+    }
 
     j=1;
-    while(kappa_tab[0][j]<log10R && j<index_R-1)
+    while(kappa_tab[0][j]<log10R && j<index_R)
         j++;
     index_R_fit = j;
-    if (index_R_fit == 1)
+
+    if (index_R_fit == 1){
+        //printf("Warning (R too small for opal kappa): log10R = %le --> %le , log10T = %le, log10rho = %le \n",log10R,kappa_tab[0][1],log10T,log10rho);
         index_R_fit = 2;
-    /* tableの外側にはバカ外挿 */
-    
+        log10R = kappa_tab[0][1];
+    } else if (index_R_fit == index_R){
+        //printf("Warning (R too large got opsl kappa): log10R = %le --> %le , log10T = %le \n",log10R,kappa_tab[0][index_R-1],log10T);
+        log10R = kappa_tab[0][index_R-1];
+    }
+
     double kappa_Tdirect_min = (kappa_tab[index_T_fit-1][index_R_fit]-kappa_tab[index_T_fit-1][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit-1][index_R_fit-1];
     double kappa_Tdirect_max = (kappa_tab[index_T_fit][index_R_fit]-kappa_tab[index_T_fit][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit][index_R_fit-1];
     double log10kappa = (kappa_Tdirect_max-kappa_Tdirect_min)/(kappa_tab[index_T_fit][0]-kappa_tab[index_T_fit-1][0])*(log10T-kappa_tab[index_T_fit-1][0])+kappa_Tdirect_min;
-    
+
     return pow(10.,log10kappa);
 }
-
--- shooting_from_rA_to_rwd_debugging.c
+++ shooting_from_rA_to_rwd_debugging.c
@@ -31,7 +31,7 @@ const int max_1sttrial = 50;
 const int max_2ndtrial = 50;
 
 /* optcaity table #46 */
-const int index_T=58;
+const int index_T=71;
 const int index_R=20;
 
 void trial_1st_stage(double dudxA, double TA, double LrA, double ln_rAmax, double ln_rAmin, double *rA_1st, double *drA_1st);
@@ -396,7 +396,7 @@ void load_kappa_table(double kappa_tab[index_T][index_R])
     int i,j;
     
     FILE *ip;
-    ip = fopen("table46.dat","r");
+    ip = fopen("tab46.dat","r");
     for (i=0; i<index_T; i++) {
         for (j=0; j<index_R; j++) {
             fscanf(ip,"%le ",&kappa_tab[i][j]);
@@ -411,26 +411,39 @@ double kappa_fit(double log10T, double log10rho, double kappa_tab[index_T][index
     /* bilinear extrapolation of the OPAL opacity table */
     int i,j,index_T_fit=0,index_R_fit=0;
     double log10R = log10rho-3.*(log10T-6.);
-    
+
     i=1;
-    while(kappa_tab[i][0]<log10T && i<index_T-1)
+    while(kappa_tab[i][0]<log10T && i<index_T)
         i++;
     index_T_fit = i;
-    if (index_T_fit == 1)
+
+    if (index_T_fit == 1){
+        //printf("Warning (T too small for opal kappa): log10T = %le --> %le , log10R = %le \n",log10T,kappa_tab[1][0],log10R);
         index_T_fit = 2;
-    
+        log10T = kappa_tab[1][0];
+    } else if (index_T_fit == index_T){
+        //printf("Warning (T too large for opal kappa): log10T = %le --> %le , log10R = %le \n",log10T,kappa_tab[index_T-1][0],log10R);
+        log10T = kappa_tab[index_T-1][0];
+    }
+
     j=1;
-    while(kappa_tab[0][j]<log10R && j<index_R-1)
+    while(kappa_tab[0][j]<log10R && j<index_R)
         j++;
     index_R_fit = j;
-    if (index_R_fit == 1)
+
+    if (index_R_fit == 1){
+        //printf("Warning (R too small for opal kappa): log10R = %le --> %le , log10T = %le, log10rho = %le \n",log10R,kappa_tab[0][1],log10T,log10rho);
         index_R_fit = 2;
-    /* tableの外側にはバカ外挿 */
-    
+        log10R = kappa_tab[0][1];
+    } else if (index_R_fit == index_R){
+        //printf("Warning (R too large got opsl kappa): log10R = %le --> %le , log10T = %le \n",log10R,kappa_tab[0][index_R-1],log10T);
+        log10R = kappa_tab[0][index_R-1];
+    }
+
     double kappa_Tdirect_min = (kappa_tab[index_T_fit-1][index_R_fit]-kappa_tab[index_T_fit-1][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit-1][index_R_fit-1];
     double kappa_Tdirect_max = (kappa_tab[index_T_fit][index_R_fit]-kappa_tab[index_T_fit][index_R_fit-1])/(kappa_tab[0][index_R_fit]-kappa_tab[0][index_R_fit-1])*(log10R-kappa_tab[0][index_R_fit-1])+kappa_tab[index_T_fit][index_R_fit-1];
     double log10kappa = (kappa_Tdirect_max-kappa_Tdirect_min)/(kappa_tab[index_T_fit][0]-kappa_tab[index_T_fit-1][0])*(log10T-kappa_tab[index_T_fit-1][0])+kappa_Tdirect_min;
-    
+
     //return 0.2;
     return pow(10.,log10kappa);
 }
